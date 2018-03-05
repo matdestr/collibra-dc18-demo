0f113e492f89323fdbd2005b1c0d4978fd642e53
Mathisse De Strooper <mathisse.destrooper@collibra.com>
2018-03-05 14:08:43 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,56 @@
+import json
+import os
+
+from flask import Flask
+from flask import make_response
+from flask import request
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    print("Request:")
+    print(json.dumps(req, indent=4))
+
+    res = makeWebhookResult(req)
+
+    res = json.dumps(res, indent=4)
+    print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+def makeWebhookResult(req):
+    if req.get("result").get("action") == "collibra.search":
+
+        # get all parameters
+        result = req.get("result")
+        parameters = result.get("parameters")
+        asset_name = parameters.get("asset")
+
+        speech = "This is a test definition for " + asset_name
+
+        print(speech)
+
+        return {
+            "speech": speech,
+            "displayText": speech,
+            # "data": {},
+            # "contextOut": [],
+            "source": "apiai-collibra-dc18-demo"
+        }
+
+    else:
+        return {}
+
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print("Starting app on port %d" % port)
+
+    app.run(debug=True, port=port, host='0.0.0.0')
