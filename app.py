71d86d6a8ad4d529cc54c8fc36eb9765c8bdc246
Mathisse De Strooper <mathisse.destrooper@collibra.com>
2018-03-08 15:47:39 +0100

    comment

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def makeWebhookResult(req):
 
         # rest call to advanced connect
         # 200
-        response = requests.request('GET', "https://53-dgc-nightly.collibra.com/rest/1.0/application/version")
+        # response = requests.request('GET', "https://53-dgc-nightly.collibra.com/rest/1.0/application/version")
         # 404
         response = requests.request('GET', "https://thebest404pageeverredux.com/swf/indx.html")
 
